4ca0fabed7578da83216a746e95180c09491c5f2
Ran Lu <ranl@princeton.edu>
2017-04-14 16:48:16 -0400

    Maybe faster

--- run_1.sh
+++ run_1.sh
@@ -22,8 +22,8 @@ try tar jcvf incomplete_edges_"$i"_"$j"_"$k".tar.bz2 --files-from /tmp/test.mani
 try aws s3 cp incomplete_edges_"$i"_"$j"_"$k".tar.bz2 s3://seunglab/region_graph_data/incomplete_edges/
 try aws s3 cp rg_volume_"$i"_"$j"_"$k".in s3://seunglab/region_graph_data/region_graphs/
 
-rm *.tar.bz2
 find . -name '*.txt' | xargs rm -v
+rm *.tar.bz2
 
 try julia rg_mmap.jl aff_chunk seg_exp_chunk $i $j $k
 try find . -name '*.txt' -print > /tmp/test.manifest
@@ -34,7 +34,7 @@ try aws s3 cp rg_volume_"$i"_"$j"_"$k".in s3://seunglab/region_graph_exp_data/re
 try touch "$i"_"$j"_"$k".txt
 try aws s3 cp "$i"_"$j"_"$k".txt s3://seunglab/region_graph_data/done/
 
-rm *.tar.bz2
 find . -name '*.txt' | xargs rm -v
+rm *.tar.bz2
 
 rm *.h5
