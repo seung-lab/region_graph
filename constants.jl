58c726132f7665456fc58e0670febbcba068baf8
Ran Lu <ranl@princeton.edu>
2017-04-14 16:47:59 -0400

    Smaller chunk size

--- constants.jl
+++ constants.jl
@@ -3,5 +3,5 @@
 const data_start = Int32[28673, 24065, 2]
 const data_end = Int32[80634, 60859, 996]
 #const data_end = data_start .+ Int32[3999,3999,995] - one(Int32)
-const chunk_size = Int32[2048, 2048, 995]
+const chunk_size = Int32[1024, 1024, 995]
 const aff_threshold = Float32(0.25)
