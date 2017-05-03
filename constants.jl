d30634643d05de7b78424b5f7bae36da8805203a
Ran Lu <ranl@princeton.edu>
2017-05-02 21:16:40 -0400

    Update the constant for the second connectum

--- constants.jl
+++ constants.jl
@@ -1,7 +1,4 @@
-#const data_start = Int32[1,1,1]
-#const data_end = Int32[2048,2048,256]
-const data_start = Int32[28673, 24065, 2]
-const data_end = Int32[80634, 60859, 996]
-#const data_end = data_start .+ Int32[3999,3999,995] - one(Int32)
-const chunk_size = Int32[1024, 1024, 995]
+const data_start = Int32[10241, 7681, 1]
+const data_end = Int32[65015, 43715, 1002]
+const chunk_size = Int32[1024, 1024, 1002]
 const aff_threshold = Float32(0.25)
