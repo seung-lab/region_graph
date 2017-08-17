782e09e29d716f5bf1aebc92c67b59502d1e0ea4
Ran Lu <ranl@princeton.edu>
2017-08-17 11:51:51 -0400

    Use explicit parameters when possible
    Not sure if it worth it any more, this is before I figured out how
    to multi-process the data

--- contact_area.jl
+++ contact_area.jl
@@ -11,7 +11,8 @@ end
 function connect_component(boundary::Set{Array{Int32,1}})
     cc_sets = Set{Array{Int32,1}}[]
     visited = Set{Array{Int32,1}}()
-    offsets = [[i,j,k] for i in -1:1 for j in -1:1 for k in -1:1]
+    offsets = [[i,j,k] for i in -one(Int32):one(Int32) for j in -one(Int32):one(Int32) for k in -one(Int32):one(Int32)]
+    neighbour = zeros(Int32,3)
     deleteat!(offsets, 14)
     for root in boundary
         if root in visited
@@ -29,10 +30,12 @@ function connect_component(boundary::Set{Array{Int32,1}})
 
             push!(visited,root)
             push!(cc, root)
-            for offset in offsets
-                neighbour = root .+ offset
+            for i in 1:26
+                neighbour[1] = root[1] + offsets[i][1]
+                neighbour[2] = root[2] + offsets[i][2]
+                neighbour[3] = root[3] + offsets[i][3]
                 if !(neighbour in visited) && neighbour in boundary
-                    enqueue!(queue, neighbour)
+                    enqueue!(queue, deepcopy(neighbour))
                 end
             end
         end
@@ -63,11 +66,13 @@ function count_edges(boundaries::Array{Dict{Array{Int32,1}, SemanticInfo},1}, bo
 
     for i in 1:3
         count = 0
-        for v in intersect(keys(boundaries[i]),boundary_cc)
-            count += 1
-            #println(boundaries[i][v])
-            sem_sum_1 += boundaries[i][v][1]
-            sem_sum_2 += boundaries[i][v][2]
+        for v in keys(boundaries[i])
+            if v in boundary_cc
+                count += 1
+                #println(boundaries[i][v])
+                sem_sum_1 += boundaries[i][v][1]
+                sem_sum_2 += boundaries[i][v][2]
+            end
         end
         append!(counts, count)
     end
