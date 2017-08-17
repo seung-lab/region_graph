fac4690562ddf4af29e873f67d68779461b95321
Ran Lu <ranl@princeton.edu>
2017-08-17 11:54:18 -0400

    Save all the boundaries to binary files
    This way we can process them in another script independently, taking
    advantage of multiple cores

--- ca_mmap.jl
+++ ca_mmap.jl
@@ -128,28 +128,24 @@ function regiongraph{Ta,Ts}(sem::Array{Ta,4},seg::Array{Ts,3}, offset::Array{Int
 
     count_edges = 0
     f_incomp = open("$(ARGS[2])/incomplete_edges_$(index[1])_$(index[2])_$(index[3]).txt", "w")
+    f_comp = open("$(ARGS[1])/complete_edges_$(index[1])_$(index[2])_$(index[3]).txt", "w")
     for p in keys(edges)
+        fn = "$(ARGS[1])/$(p[1])_$(p[2])_$(index[1])_$(index[2])_$(index[3]).txt"
         if p[1] in incomplete_segments && p[2] in incomplete_segments
-            push!(boundary_edges, p)
-            open("$(ARGS[2])/$(p[1])_$(p[2])_$(index[1])_$(index[2])_$(index[3]).txt", "w") do f
-                for i in 1:3
-                    for k in keys(edges[p].boundaries[i])
-                        write(f, "$i $(k[1]+offset[1]-one(Int32)) $(k[2]+offset[2]-one(Int32)) $(k[3]+offset[3]-one(Int32)) $(Float64(edges[p].boundaries[i][k][1][1])) $(Float64(edges[p].boundaries[i][k][1][2])) $(Float64(edges[p].boundaries[i][k][1][3])) $(Float64(edges[p].boundaries[i][k][1][4])) $(Float64(edges[p].boundaries[i][k][2][1])) $(Float64(edges[p].boundaries[i][k][2][2])) $(Float64(edges[p].boundaries[i][k][2][3])) $(Float64(edges[p].boundaries[i][k][2][4]))\n")
-                    end
+            fn = "$(ARGS[1])/$(p[1])_$(p[2])_$(index[1])_$(index[2])_$(index[3]).txt"
+            write(f_incomp, "$(p[1]) $(p[2])\n")
+        else
+            write(f_comp, "$(p[1]) $(p[2])\n")
+        end
+        open(fn, "w") do f
+            for i in 1:3
+                for k in keys(edges[p].boundaries[i])
+                    write(f, i)
+                    write(f, (k+offset-one(Int32)))
+                    write(f, edges[p].boundaries[i][k][1])
+                    write(f, edges[p].boundaries[i][k][2])
                 end
             end
-            write(f_incomp, "$(p[1]) $(p[2])\n")
-            count_edges+=1
-            continue
-        end
-        push!(edges_array, p)
-    end
-    complete_edge_output = Array{Array{Float64,1},1}(length(edges_array))
-    @threads for i in 1:length(edges_array)
-        complete_edge_output[i] = zeros(Float64, 4)
-        process_edge!(edges_array[i], edges[edges_array[i]], complete_edge_output[i])
-        if i % 100000 == 0
-            println("process complete edge: $i")
         end
     end
     #for i in 1:length(edges_array)
@@ -157,7 +153,7 @@ function regiongraph{Ta,Ts}(sem::Array{Ta,4},seg::Array{Ts,3}, offset::Array{Int
     #end
     close(f1)
     close(f_incomp)
-    println("boundary segments: $(length(boundary_edges)), edges: $(count_edges)")
+    close(f_comp)
 end
 
 index = [parse(Int32, x) for x in ARGS[3:end]]
